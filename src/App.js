2668f1ce33ea2f38b276d23fb349f991ad34407e
Jun Mao <mr.jun.mao@gmail.com>
2019-10-10 11:56:28 -0700

    implement weather

--- src/App.js
+++ src/App.js
@@ -2,13 +2,108 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class SnapShot extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    //this.state = Object.assign(props.values);
+    //console.log(this.state);
+    /*{
+      date: new Date(data.dt * 1000).toLocaleDateString(),
+      high: data.main.temp_max,
+      low: data.main.temp_min,
+      desc: data.weather[0].description
+    }*/
+  }
+
+  render() {
+    let list = "";
+    if (this.props.data) {
+      list = this.props.data.map((item, index) =>
+        <li key={index}>
+          <div className="item">
+            <div>Date : {new Date(item.dt * 1000).toUTCString()}</div>
+            <div>High temp: {item.main.temp_max}</div>
+            <div>Low : {item.main.temp_min}</div>
+            <div>Description : {item.weather[0].description}</div>
+          </div>
+        </li>);
+    }
+
+    return (
+      <ul className="list">{list}</ul>
+    );
+  }
+
+}
+class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { city: 'san diego', period: "current" };
+    this.handleCityChange = this.handleCityChange.bind(this);
+    this.handlePeriodChange = this.handlePeriodChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleCityChange(event) {
+    this.setState({ city: event.target.value });
+  }
+
+  handlePeriodChange(event) {
+    this.setState({ period: event.target.value });
+  }
+
+  handleSubmit(event) {
+    this.update();
+    event.preventDefault();
+  }
+
+  update() {
+    fetch(`http://api.openweathermap.org/data/2.5/${this.state.period === 'current' ? 'weather' : 'forecast'}?q=${this.state.city}&` +
+      `APPID=c6b0b2b0c00ef5d8be8850e1fa6f375c`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        if (this.state.period === 'current') {
+          this.setState({ data: [data] });
+        }
+        else {
+          this.setState({ data: data.list });
+        }
+      });
+
+  }
+
+  componentDidMount() {
+    this.update();
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" value={this.state.city}
+          onChange={this.handleCityChange} />
+        <select value={this.state.period} onChange={this.handlePeriodChange}>
+          <option value="current">Current</option>
+          <option value="5_day">5 day</option>
+        </select>
+        <button onClick={this.handleSubmit}>Go</button>
+        <SnapShot {...this.state} />
+      </div>
+    );
+  }
+}
+
 function App() {
   return (
+    <Weather />
+  );
+  /*return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload1.
         </p>
         <a
           className="App-link"
@@ -20,7 +115,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  );*/
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Weather from './App';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import TestRenderer from 'react-test-renderer';
+import { exportAllDeclaration } from '@babel/types';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('try it out', () => {
+
+  const testRenderer = TestRenderer.create(<Weather />);
+  const testInstance = testRenderer.root;
+  expect(testInstance.findByType('input').props.value).toBe('san diego');
+
 });
